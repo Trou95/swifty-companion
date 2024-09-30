0e834916bcc2329315dd301175fc3eba2ac3bec5
Trou95 <gorkem_095@hotmail.com>
2024-09-30 18:42:30 +0300

    Update

--- src/app/(swifty)/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// To handle a GET request to /api
-export async function POST(request: Request) {
-  //Get code from request
-  console.log('GET request');
-  //Return a response
-  return NextResponse.json({ '123': '123' }, { status: 200 });
-}
