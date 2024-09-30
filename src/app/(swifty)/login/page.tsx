16590dc714bd784d84d72eafa277b9709ac53b03
Trou95 <gorkem_095@hotmail.com>
2024-09-30 16:42:19 +0300

    Update

--- src/app/(swifty)/login/page.tsx
+++ src/app/(swifty)/login/page.tsx
@@ -22,17 +22,38 @@ export default function LoginPage() {
         console.log(process.env.NEXT_PUBLIC_AUTH_URL);
         window.location.href = process.env.NEXT_PUBLIC_AUTH_URL!;
       } else {
-        console.log('123');
-        axios.Authenticate(code).then((res) => {
-          console.log('token', res.data);
-          axios.setToken(res.data.access_token);
-          setAccessToken(res.data.access_token);
-          BrowserAPI.setTokens(res.data.access_token, res.data.refresh_token);
-          axios.getUser().then((res) => {
-            const user = mapUser(res.data);
-            setUser(user);
-          });
-        });
+        fetch(process.env.NEXT_PUBLIC_INTRA_AUTH_URL!, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+            client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            code: code,
+            grant_type: 'authorization_code',
+            redirect_uri: 'https://swifty-companion.vercel.app/login',
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('token', data);
+            setAccessToken(data.access_token);
+            BrowserAPI.setTokens(data.access_token, data.refresh_token);
+
+            fetch('https://api.intra.42.fr/v2/me', {
+              method: 'GET',
+              headers: {
+                Authorization: `Bearer ${data.access_token}`,
+              },
+            })
+              .then((response) => response.json())
+              .then((userData) => {
+                const user = mapUser(userData);
+                setUser(user);
+              });
+          })
+          .catch((error) => console.error('Error:', error));
       }
     }
   }, [user]);
