24874b510dd40d1d4856d87857a9006f779760dd
Trou95 <gorkem_095@hotmail.com>
2024-10-02 00:41:30 +0300

    Updated .gitignore && Removed capacitor.config

--- swifty-companion-web/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'swifty-companion',
-  webDir: 'out',
-};
-
-export default config;
