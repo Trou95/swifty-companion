72130f960850a686ce3ebe311f66aaaa7e9560de
Trou95 <gorkem_095@hotmail.com>
2024-10-02 19:33:02 +0300

    Update

--- swifty-companion-web/src/app/(swifty)/account/page.tsx
+++ swifty-companion-web/src/app/(swifty)/account/page.tsx
@@ -49,8 +49,8 @@ export default function AccountPage() {
 
   const handleLogout = () => {
     BrowserAPI.clearTokens().then(() => {
-      push('/')
       setUser(null);
+      push('/');
     });
   };
 
